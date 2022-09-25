44f7982c72a2a80589db42e6158d1ee518fc4b2a
gjxo12 <vhxpffltm@naver.com>
2022-09-25 21:42:40 +0900

    chapter7: Variable Argument commplete

--- chapter7/variableArg/main.cpp
+++ chapter7/variableArg/main.cpp
@@ -25,6 +25,28 @@ void Func_str(int vas,...){
     std::cout << str << "\n";
 }
 
+class CTest
+{
+    public:
+    CTest() {m_V = 0;}
+    CTest(const CTest& obj) {m_V = obj.m_V + 1;}
+    int m_V;
+};
+
+void CFunc(CTest arg){ // 고정 인자 함수?
+    CTest t = arg;  // 여기서는 어떤 생성자의 호출인가?
+    std::cout << "CFunc: " << t.m_V << "\n";
+}
+
+void VAFunc(int vas, ...)
+{
+    va_list arg_ptr;
+    va_start(arg_ptr, vas);
+    CTest t = va_arg(arg_ptr, CTest); //
+    va_end(arg_ptr);
+    std::cout << "VAFunc: " << t.m_V << "\n";
+}
+
 int main()
 {
     char a1 = '1';
@@ -36,4 +58,18 @@ int main()
     std::string str = "String..";
     Func_str(0,str.c_str());
     // string 객체넘길때 주의
+
+    CTest t;
+    CFunc(t);     // 여기서는 어떤 생성자의 호출인가?
+    VAFunc(0,t);
+    // 가변 인자는 값에 의한 호출방식으로 전달,
+    // 복사 생성자가 호출되지 않음, 오직 메모리 복사만 발생
+
+    return 0;
 }
+
+/*
+* 호출 규약에 따른 스택정리
+* cdecl / stdcall: 이 둘의 차이는 전달된 인자의 스택 정리를 누가 담당하는가?
+* cdecl: caller  stdcall: callee -> 절대로 가변인자 함수를 만들수 없음
+*/
