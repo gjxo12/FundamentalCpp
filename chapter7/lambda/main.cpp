5123e39a68dd1020ea1a2a2583ddd4e9309b7e04
gjxo12 <vhxpffltm@naver.com>
2022-10-23 22:06:43 +0900

    chapter7: Lambda with Template

--- chapter7/lambda/main.cpp
+++ chapter7/lambda/main.cpp
@@ -1,9 +1,23 @@
 #include "chapter7/lambda/lambda.h"
 #include "chapter7/lambda/CFunction.h"
 
+using chapter7::CFunction::CFunction;
+using chapter7::CFunction::Func;
 using chapter7::lambda::CLambda;
 using chapter7::lambda::CTest;
-using chapter7::CFunction::Func;
+
+CFunction<double(int, int)> lambda;
+CFunction<double(int, int)> func;
+// 이같이 람다오ㅘ 함수저장을 위한 CFunction 객체 두개
+
+double Divide(int a, int b)
+{
+    if (b != 0)
+    {
+        return (double)a / b;
+    }
+    return 0;
+}
 
 int main()
 {
@@ -19,8 +33,25 @@ int main()
     // 람다의 저장
     chapter7::lambda::Func();
 
-    //람다 저장 클래스
-    Func([](int arg){std::cout << arg << "\n";});// 실제 대입된 람다의 타입이 됨..
+    // 람다 저장 클래스
+    Func([](int arg)
+         { std::cout << arg << "\n"; }); // 실제 대입된 람다의 타입이 됨..
+    // 함수 시그니처: 기본적으로 반환 타입과 인자들의 타입까지 모아놓은 정보
+
+    // 람다 템플릿.. CFunction
+    double vx = 0.3;
+    double rx = 0;
+
+    lambda = [vx, &rx](int a, int b) -> double
+    {
+        rx = 0.4;
+        return vx + rx + a + b;
+    };
+    func = &Divide;
+    // 람다와 함수를 대입 받아서 사용가능..
+    std::cout << lambda(1, 2) << "\n";
+    std::cout << func(1, 2) << "\n";
+
 
     return 0;
 }
--- include/chapter7/lambda/CFunction.h
+++ include/chapter7/lambda/CFunction.h
@@ -11,7 +11,8 @@ namespace chapter7
             TEST lambda = arg; // TEST 타입은 구현쪽에서 실제 대입된 타입으로 됨..
             lambda(1);
         }
-        
+
+        // std::function의 인터페이스를 구현
         template <typename RT, typename... ATs>
         class CLammbdaInterface // 람다 인터페이스
         {
@@ -21,11 +22,16 @@ namespace chapter7
             virtual RT _Call(ATs... args) = 0;
         };
 
+        // 2) 실제 람다가 저장될 클래스
         template <typename LAMBDA, typename RT, typename... ATs>
         class CLambda : public CLammbdaInterface<RT, ATs...>
         {
         public:
             CLambda(const LAMBDA &lambda) { m_pLambda = new LAMBDA(lambda); }
+            // 복사 생성자: 명시적 클래스가 아니므로 기본 생성자가 없고
+            // 암시적 복사 생성자는 사용가능
+            // stack에 생성되는 임시 객체이니 new를 통한 복사 생성을 통해 힙에 저장하도록 함
+            // 이것으로 CFunction에서 람다를 저장할수 있음..
             ~CLambda()
             {
                 if (m_pLambda)
@@ -37,11 +43,12 @@ namespace chapter7
             {
                 return (*m_pLambda)(args...);
             }
-            LAMBDA *m_pLambda;
+            LAMBDA *m_pLambda; // 실제로 저장하는 클래스이니 LAMDA 타입이 꼭 필요
         };
 
+        // 1) 대표 클래스
         template <typename T>
-        class CFunction; // CFunction<T> 전방 선언, 아래에서 람다 시그니처가 템플릿 인자로 대입되는 클래스를 정의하기 위함
+        class CFunction; // CFunction<T> 전방 선언, 아래에서 람다 시그니처가 템플릿 인자로 대입되는 클래스를 정의(CFunction<RT(ATs...)>)하기 위함
 
         template <typename RT, typename... ATs>
         class CFunction<RT(ATs...)>
@@ -56,7 +63,7 @@ namespace chapter7
             }
 
             template <typename LAMBDA>
-            CFunction<RT(ATs...)> &operator(const LAMBDA &lambda)
+            CFunction<RT(ATs...)>&operator=(const LAMBDA &lambda)
             {
                 if (m_pLambdaInterface)
                 {
@@ -66,14 +73,16 @@ namespace chapter7
                 m_pLambdaInterface = new CLambda<LAMBDA, RT, ATs...>(lambda);
                 return *this;
             }
+            // 복사 생성자나 복사 대입 연산자를 통해 CFunction 객체에 LAMBDA가 대입되는 순간 람다타입 알수있음..
 
             RT operator()(ATs... args) { return m_pLambdaInterface->Call(args...); }
+            // CFunction 클래스는 람다 객체를 내부에 저장하고 있는 함수 객체(functor)인것..
 
             CLammbdaInterface<RT, ATs...> *m_pLambdaInterface; // CFunction은 CLambdaInterface 포인터를 맴버로
             // 람다를 저장하기 위해.. CLambda*로는 직접 저장할수가 없음..
             // 람다 타입을 알아야하는데ㅔ.. CFunction을 정의하는 시점에 전달할 수 있는 템플릿 인자는..
             // 오직 함수의 시그니처 뿐..
-        }
+        };
 
     }
 }
