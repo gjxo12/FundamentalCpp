4839847390e451b9f8444da7581c375f7a8e76f6
gjxo12 <vhxpffltm@naver.com>
2022-12-01 00:13:12 +0900

    chapter8: multiinheritvirtual class memory example

--- chapter8/multiinheritvirtual/multiinheritvirtual.cpp
+++ chapter8/multiinheritvirtual/multiinheritvirtual.cpp
@@ -3,6 +3,11 @@
 
 using chapter8::multiinheritvirtual::CChild;
 using chapter8::multiinheritvirtual::CParent;
+using chapter8::multiinheritvirtual::CParent1;
+using chapter8::multiinheritvirtual::CParent2;
+using chapter8::multiinheritvirtual::CParent3;
+using chapter8::multiinheritvirtual::CParent4;
+using chapter8::multiinheritvirtual::CChild2;
 
 int main()
 {
@@ -13,5 +18,18 @@ int main()
     // 왜 다른지 꼭 이해해야한다
     std::cout << pChild << "  " << pParent << "\n";
 
+
+    CChild2 c2;
+    CChild2* pC = &c2;
+    CParent1* p1 = pC;
+    CParent2* p2 = pC;
+    CParent3* p3 = pC;
+    CParent4* p4 = pC;
+
+    std::cout << pC << "  " << p1 << "  " << p2 << "  " << p3 << "  " << p4 << "\n";
+    // 결과 생각해보기..
+    // 메모리 구조가 상식을 깸.. 확실한건 vfptr이 가장 위.. 어떤 vfptr일까?
+    // CChild2의 vfptr은 몇개일까? 가지고 있을까?
+    // GCC도 똑같을까..? -> 중요한것은 main vfptr은 무조건 처음에 나타난다는것!!
     return 0;
 }
--- include/chapter8/multiinheritvirtual/multiinheritvirtual.h
+++ include/chapter8/multiinheritvirtual/multiinheritvirtual.h
@@ -1,6 +1,11 @@
 #ifndef __MULTIINHERITVIRTUAL__
 #define __MULTIINHERITVIRTUAL__
 
+/**
+ * 다중 상속에는 vfptr이 여러개 있을수 있다.
+ * 항상 main vfptr이 정해지고 이는 클래스 메모리 구조의 시작위치에 놓인다
+ */
+
 namespace chapter8
 {
     namespace multiinheritvirtual
@@ -18,6 +23,38 @@ namespace chapter8
         };
         // 컴파일러가 메모리 구조를 어떻게 나타낼까..?
 
+        class CParent1
+        {
+        public:
+            int m_Parent1;
+        };
+        class CParent2
+        {
+        public:
+            int m_Parent2;
+            virtual void VFunc2() = 0;
+        };
+        class CParent3
+        {
+        public:
+            int m_Parent3;
+        };
+        class CParent4
+        {
+        public:
+            virtual void VFunc4() = 0;
+            int m_Parent4;
+        };
+        class CChild2 : public CParent1, public CParent2, public CParent3, public CParent4
+        {
+        public:
+            virtual void VFunc2() {}
+            virtual void VFunc4() {}
+            virtual void VFuncC() {}
+            int m_Child;
+        };
+        // 상속순서와 메모리 위치는 상관관계가 있으나 일치하지는 않는다
+        // 생성자의 호출 순서는 1 2 3 4 이지만 메모리 순서는 다를것
     }
 }
 
